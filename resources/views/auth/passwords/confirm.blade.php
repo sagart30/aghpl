1e6f7e3f59d747d7c43d7e9ca82361d053c04eea
Sagar Thakur <sagarthakur30@gmail.com>
2020-04-22 15:53:50 +0530

    Login module css and js

--- resources/views/auth/passwords/confirm.blade.php
+++ resources/views/auth/passwords/confirm.blade.php
@@ -1,4 +1,8 @@
 @extends('layouts.app')
+  <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+  <script src="{{ asset('js/jquery.min.js') }}"></script>
+  <script src="{{ asset('js/popper.min.js') }}"></script>
+  <script src="{{ asset('js/bootstrap.min.js') }}"></script>
 
 @section('content')
 <div class="container">
